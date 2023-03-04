5a315a3133d7c90632a342559ab995a487c5e1bc
Daniel Brott <danbrott42@gmail.com>
2023-03-04 18:38:52 +0000

    code refactor and readme update

--- rmp_scrape/config.py
+++ rmp_scrape/config.py
@@ -3,7 +3,7 @@
 
 testing = True
 # sid = 1256  # University of Wisconsin-Madison SID
-sid = 186 # CWRU SID
-page_reload_timeout = 100 # 100 seconds
+sid = 1224 # CWRU SID
+page_reload_timeout = 10 # 100 seconds
 show_more_timeout = 10 # 10 seconds
 file_path = 'all_professors.json'
